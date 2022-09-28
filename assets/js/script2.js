ef3775a9b6c58459c72a81bda86c6d237224dc54
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-09-28 14:39:17 -0500

    added data api

---
+++ assets/js/script2.js
@@ -0,0 +1,34 @@
+const key = "VWy53BSbhb6aR1M1ke08svAEF1FEdVeANJN9WnQRcFKg3hO0qU";
+const secret = "ZktvYuiXVzJmlliMIS5MRtEw27rAmvPtpNFbxrh1";
+
+// Update token
+fetch("https://api.petfinder.com/v2/oauth2/token", {
+  method: "POST",
+  body:
+    "grant_type=client_credentials&client_id=" +
+    key +
+    "&client_secret=" +
+    secret,
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data.access_token);
+    const token = data.access_token;
+
+    fetch(`https://api.petfinder.com/v2/animals?type=dog&page=`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  });
