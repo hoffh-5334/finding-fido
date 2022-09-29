772ed74a1af48e8a314ad250d3791e231c9f1cca
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-09-29 12:49:45 -0500

    added cards

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,7 +1,9 @@
 const key = "VWy53BSbhb6aR1M1ke08svAEF1FEdVeANJN9WnQRcFKg3hO0qU";
 const secret = "ZktvYuiXVzJmlliMIS5MRtEw27rAmvPtpNFbxrh1";
 
-// Update token
+const searchResults = [];
+
+// Update token and fetch search results
 const getData = function() {
 fetch("https://api.petfinder.com/v2/oauth2/token", {
   method: "POST",
@@ -18,10 +20,8 @@ fetch("https://api.petfinder.com/v2/oauth2/token", {
     return response.json();
   })
   .then((data) => {
-    console.log(data.access_token);
     const token = data.access_token;
-
-    fetch(`https://api.petfinder.com/v2/animals?type=dog&page=`, {
+    fetch("https://api.petfinder.com/v2/animals?type=dog&page=1", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -30,7 +30,44 @@ fetch("https://api.petfinder.com/v2/oauth2/token", {
         return res.json();
       })
       .then((data) => {
+        console.log(data)
+        data.animals.forEach(function(item){
+          searchResults.push(item)
+        })
+        updateCard()
         return data
       });
   });
 };
+
+// Update card values
+const updateCard = function(){
+  console.log("update")
+  console.log(searchResults)
+  searchResults.forEach(function(item){
+    // $('.card').attr("style", "width: 18rem;")
+
+
+    // old method
+    $(".card-img-top").attr("src", item.photos[0].small)
+    $(".card-title").text(item.name)
+    $(".card-text").text(item.description)
+
+    $(".list-group").append($("<li class='list-group-item'>").text(`Gender: ${item.gender}`))
+    $(".list-group").append($("<li class='list-group-item'>").text(`Breed: ${item.breeds.primary}`))
+
+    console.log(item.contact.email)
+    // Contact info
+    $("#contact-info").append($("<a class='card-link'>").text(item.contact.email).attr("href", item.contact.email))
+    $("#contact-info").append($("<a class='card-link'>").text(item.contact.phone).attr("href", item.contact.phone))
+   
+
+  })
+}
+
+// Run at start
+const init = function(){
+  getData()
+}
+
+init()
