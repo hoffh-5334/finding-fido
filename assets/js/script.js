ffbacd64bb24c6e9cba15957085ee75590611b3a
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-09-30 10:31:50 -0500

    update api

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ const newUrl = function(){
     // Host will need to be updated before deploying to GitPages
     const host = "http://127.0.0.1:5500/projects/group-project1";
 
-    window.open(`${host}/2index.html?location=${zipCode}&age=${age}&size=${size}&gender=${gender}&good_with_dogs=${goodWithOtherDogs}&good_with_cats=${goodWithCats}&good_with_children=${goodWithChildren}`)
+    window.open(`${host}/2index.html?location=${zipCode}&age=${age}&size=${size}&gender=${gender}&good_with_dogs=${goodWithOtherDogs}&good_with_cats=${goodWithCats}&good_with_children=${goodWithChildren}`,"_self")
 }
 
 $("#search-form").on("submit", function(e){
