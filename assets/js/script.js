ffebe4de05aceb7ea84f2c13a44855fac77cc65d
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-10-01 00:28:04 -0500

    fixed url function and added search results bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,36 @@
-// Form input variables
-let zipCode;
-let age;
-let size;
-let gender;
-let goodWithOtherDogs;
-let goodWithCats;
-let goodWithChildren;
-
-
-// Create new URL
-const newUrl = function(){
-    // Host will need to be updated before deploying to GitPages
-    const host = "http://127.0.0.1:5500/projects/group-project1";
-
-    window.open(`${host}/2index.html?location=${zipCode}&age=${age}&size=${size}&gender=${gender}&good_with_dogs=${goodWithOtherDogs}&good_with_cats=${goodWithCats}&good_with_children=${goodWithChildren}`,"_self")
-}
-
-$("#search-form").on("submit", function(e){
+// Event listener for search form
+$("#search-form").on("submit", function (e) {
     e.preventDefault()
-    console.log("search")
-    zipCode = $("#zip-code").val();
-    age = $("#age-input").val();
-    size = $("#size-input").val();
-    gender = $("#gender-input").val();
-    goodWithOtherDogs = $("#good-with-dogs").val()
-    goodWithCats = $("#good-with-cats").val()
-    goodWithChildren = $("#good-with-children").val()
-   
-    newUrl()
 
+    // Url input/api param object
+    const paramObj = {
+        location: $("#zip-code").val(),
+        age: $("#age-input").val(),
+        size: $("#size-input").val(),
+        gender: $("#gender-input").val(),
+        good_with_dogs: $("#good-with-dogs").val(),
+        good_with_cats: $("#good-with-cats").val(),
+        good_with_children: $("#good-with-children").val()
+    }
+
+    // Host will need to be updated before deploying to GitPages
+    const host = "http://127.0.0.1:5500/projects/group-project1/2index.html?";
+
+    // Filter out nulls and URLSearchParams object to create new URL
+    var newParams = Object.entries(paramObj).filter(item => {
+        if (item[1] !== null && item[1] !== "null") {
+            return item;
+        }
+    })
+
+    // Parse and clean new parameters for the url
+    var paramString = newParams.map(item => {
+        return item[0] + "=" + item[1]
+    }).join("&");
+
+    // Filter out nulls values then use URLSearchParams object to create new URL
+    const newURL = host + new URLSearchParams(paramString)
+
+    // Open new page with search params
+    window.open(newURL, "_self");
 })
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -2,75 +2,124 @@ const key = "VWy53BSbhb6aR1M1ke08svAEF1FEdVeANJN9WnQRcFKg3hO0qU";
 const secret = "ZktvYuiXVzJmlliMIS5MRtEw27rAmvPtpNFbxrh1";
 
 const searchResults = [];
-const urlParams = window.location.search
+let pagination = null;
 
-// Update token and fetch search results
-const getData = function(urlParams) {
-fetch("https://api.petfinder.com/v2/oauth2/token", {
-  method: "POST",
-  body:
-    "grant_type=client_credentials&client_id=" +
-    key +
-    "&client_secret=" +
-    secret,
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-})
-  .then((response) => {
-    return response.json();
+// PetFinder POST API
+const getData = async function () {
+  let options = {
+    method: "POST",
+    body:
+      "grant_type=client_credentials&client_id=" +
+      key +
+      "&client_secret=" +
+      secret,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    }
+  };
+  const response = await fetch("https://api.petfinder.com/v2/oauth2/token", options);
+  const token = await response.json();
+  const searchRes = await fetch(`https://api.petfinder.com/v2/animals${window.location.search}&type=dog&page=1&`, {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+    },
   })
-  .then((data) => {
-    const token = data.access_token;
-    // console.log(urlParams)
-    fetch(`https://api.petfinder.com/v2/animals${urlParams}&type=dog&page=1&`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => {
-        return res.json();
+  if (searchRes.status < 400) {
+    const data = await searchRes.json()
+
+    // If there are results, push to array, else render no results html
+    if (data.animals.length > 0) {
+      console.log(data);
+
+      // Clear card if no api error
+      await clearCards();
+
+      // Update search results array
+      data.animals.forEach(function (item) {
+        searchResults.push(item);
       })
-      .then((data) => {
-        console.log(data)
-        data.animals.forEach(function(item){
-          searchResults.push(item)
-        })
-        // Handle return of no data
-        if(data.animals.length > 0) {
-          updateCard()
-        } else {
-          console.log("No search results")
-        }
-      });
-  });
+
+      // Set pagination object to get total
+      pagination = data.pagination;
+
+      // Update components after search
+      updateSearchBar();
+      updateCard();
+
+    } else {
+      console.log("No data returned for filter option")
+    }
+  } else {
+    console.log(searchRes)
+  }
+}
+
+/** 
+ * Append elements function
+ * @param {string} tag: element type 
+ * @param {string} content: text value of the element
+ * @param {array} attr: pass an array with attr and value. Ex: [class, "search-bar"] 
+ * @param {string} appendTo: Parent element to append to
+ */
+const renderElement = (tag, content = null, attr = null, appendTo,) => {
+  const elem = $(tag);
+  content != null ? elem.text(content) : "";
+  attr != null ? elem.attr(attr[0], attr[1]) : "";
+  typeof appendTo === "string" ? $(appendTo).append(elem) : appendTo.append(elem);
 };
 
+// Search bar results
+const renderSearchBar = function () {
+  $("#search-header").css({
+    backgroundColor: "#fff",
+    boxShadow: "0px 0px 5px 2px rgba(0,0,0,.1)",
+    gap: "1rem",
+    padding: "1rem",
+    margin: "1rem"
+  })
+  renderElement("<div>", `Results: ${0}`, ["id", "total-count"], "#search-header")
 
-// Create handle no search results function
+}
 
+const updateSearchBar = function () {
+  $("#search-header").empty()
+  let totalResults = 0;
+  if (pagination === null) {
+    totalResults = 0;
+  } else {
+    totalResults = pagination.total_count
+  }
+  renderElement("<div>", `Results: ${searchResults.length * pagination.current_page} of ${totalResults}`, ["id", "total-count"], "#search-header")
+}
+
+const clearCards = async function () {
+  $("#card-wrapper").html("");
+  searchResults.length = 0;
+}
 
 // Update card values
-const updateCard = function(){
-  searchResults.forEach(function(item){
+const updateCard = function () {
+  searchResults.forEach(function (item) {
     // Card Component
     const card = $("<div class='col-3 card'>");
     const cardBodyDisc = $("<div class='card-body'>");
     const infoList = $("<ul class='list-group list-group-flush'>");
-    const cardBodyContact = $("<div class='card-body'>");
-    
+    const cardBodyContact = $("<div class='card-body contact-info'>");
+
     // Search data components
-    const dogPhoto = $("<img class='card-img-top'>").attr("src", item.photos[0].full);
+    let dogPhoto = $("<span>");
+    item.photos.length ? dogPhoto = $("<img class='card-img-top'>").attr("src", item.photos[0].full) : dogPhoto = $("<img class='card-img-top'>").attr("src", "./assets/images/finding-fido-logo.png");
     const dogName = $("<h5 class='card-title'>").text(item.name);
-    const descriptionEl = $("<p class='card-text'>").text(item.description);
+    let descriptionEl = $("<span>");
+    item.description === null ? descriptionEl = $("<p class='card-text'>").text(`No info for ${item.name}.`) : descriptionEl = $("<p class='card-text'>").text(item.description);
     const genderEl = $("<li class='list-group-item'>").text(`Gender: ${item.gender}`);
     const breedEl = $("<li class='list-group-item'>").text(`Breed: ${item.breeds.primary}`);
     const contactInfoDivE = $("<div class=''>");
     const contactInfoDivP = $("<div class=''>");
     const emailEl = $("<a class='card-link'>").text(`Email: ${item.contact.email}`).attr("href", `mailto:${item.contact.email}`)
     const phoneEl = $("<a class='card-link'>").text(item.contact.phone).attr("href", item.contact.phone)
-    const favButton =$("<button class= 'fav'>").text("favorite")
-    favButton.click((event)=>saveFav(event, item))
+    const favButton = $("<button class= 'fav'>").text("favorite")
+    favButton.click((event) => saveFav(event, item))
 
     // Append Card Components
     $("#card-wrapper").append(card);
@@ -86,54 +135,47 @@ const updateCard = function(){
     card.append(infoList);
     infoList.append(genderEl);
     infoList.append(breedEl);
-   
+
     // Contact list
     card.append(cardBodyContact);
     cardBodyContact.append(contactInfoDivE);
     cardBodyContact.append(contactInfoDivP);
     contactInfoDivE.append(emailEl);
     contactInfoDivP.append(phoneEl);
-    contactInfoDivP.append(favButton); 
-
-    
-    
-    // // old method
-    // $(".card-img-top").attr("src", item.photos[0].small)
-    // $(".card-title").text(item.name)
-    // $(".card-text").text(item.description)
-
-    // $(".list-group").append($("<li class='list-group-item'>").text(`Gender: ${item.gender}`))
-    // $(".list-group").append($("<li class='list-group-item'>").text(`Breed: ${item.breeds.primary}`))
-
-    // console.log(item.contact.email)
-    // // Contact info
-    // $("#contact-info").append($("<a class='card-link'>").text(item.contact.email).attr("href", item.contact.email))
-    // $("#contact-info").append($("<a class='card-link'>").text(item.contact.phone).attr("href", item.contact.phone))
+    contactInfoDivP.append(favButton);
   })
 }
-const saveFav = function (event, item){
+
+const saveFav = function (event, item) {
   event.preventDefault()
   const favorites = JSON.parse(localStorage.getItem("favorites")) || []
   favorites.push({
-   name: item.name,
-   image: item.photos[0].medium, 
+    name: item.name,
+    image: item.photos[0].medium,
   })
-localStorage.setItem("favorites",JSON.stringify(favorites))
+  localStorage.setItem("favorites", JSON.stringify(favorites))
   displayFav()
-  console.log (item)
+  console.log(item)
 
 }
 // Run at start
-const init = function(){
-  getData(urlParams)
+const init = function () {
+  // Renders some of the page elements created using JS
+  renderSearchBar()
+
+  if (window.location.search.length > 0) {
+    getData()
+  } else {
+    console.log("No Search params")
+  }
 }
 
-const displayFav = function(){
+const displayFav = function () {
   const favorites = JSON.parse(localStorage.getItem("favorites")) || []
   console.log(favorites)
   const cards = $(".cards")
   cards.empty()
-  favorites.forEach(dog=>{
+  favorites.forEach(dog => {
     console.log(dog.image)
     const card = $("<div class='col-3 card'>");
     const dogName = $("<h5>").text(dog.name);
@@ -142,8 +184,50 @@ const displayFav = function(){
     card.append(dogName)
     card.append(dogImage)
     cards.append(card)
-  }) 
+  })
 }
 
 init()
 displayFav()
+
+$("#search-form").on("submit", function (e) {
+  e.preventDefault()
+
+  // API param and form input key value pair
+  const paramObj = {
+    location: $("#zip-code").val(),
+    age: $("#age-input").val(),
+    size: $("#size-input").val(),
+    gender: $("#gender-input").val(),
+    good_with_dogs: $("#good-with-dogs").val(),
+    good_with_cats: $("#good-with-cats").val(),
+    good_with_children: $("#good-with-children").val()
+  }
+
+  // Filter out nulls and URLSearchParams object to create new URL
+  var newParams = Object.entries(paramObj).filter(item => {
+    if (item[1] !== null && item[1] !== "null") {
+      return item;
+    }
+  })
+
+  // Parse and clean new parameters for the url
+  var paramString = newParams.map(item => {
+    return item[0] + "=" + item[1]
+  }).join("&");
+
+  // Push new params to url without reloading
+  history.pushState("", "", "2index.html?" + paramString);
+
+  // Run search api
+  getData();
+});
+
+// // Save for testing with multiple params. Can delete later
+// const testParam = [['location', '55418'], ['age', 'young'], ['gender', 'female'], ['good_with_dogs', 'true'], ['breeds', 'pug,samoyed']]
+// console.log(testParam)
+
+// var newTest = testParam.map(item => {
+//   return item[0] + "=" + item[1]
+// })
+// console.log(newTest.join("&"))
